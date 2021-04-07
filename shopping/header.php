3c275557deb7773a5adc3f3b16a9703bc4b18a97
yuvarajkondapalle <66169949+yuvarajkondapalle@users.noreply.github.com>
2021-04-07 14:25:39 +0530

    Create header.php

---
+++ shopping/header.php
@@ -0,0 +1,20 @@
+<header>
+        <h5>Logo Here</h5> 
+        <div id="searchbar">
+        <a href="">All Categories <i class="fa fa-caret-down "></i></a>
+        <input type="text" placeholder="Search here..">
+        <button  type="submit" ><i class="fas fa-search" style="color: white; font-size: 20px;" ></i></button>
+        </div>
+        <div id="profile">
+        <img src="Icons/Navigation icons/svg/profile icon.svg" alt="profile icon" style="padding-top: 5px;" >
+        <a href="">Sign in<b> <br> My Account</b></a>
+        </div>
+        <div id="track">
+            <img src="Icons/Navigation icons/svg/Location.svg" alt="Location icon" style="padding-top: 5px;" >
+            <a href="">Track<b> <br> Order</b></a>
+        </div>
+        <div id="cart">
+            <img src="Icons/Navigation icons/svg/Cart.svg" alt="cart icon"  style="padding-top: 5px;" >
+            <a href="" style="position: absolute; top:10px;"> <b> Your Order</b> </a>
+        </div>
+</header>
