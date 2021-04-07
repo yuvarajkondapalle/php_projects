44ab6c1e3b29135ca29e4f9aef4ebf715a83c1e0
yuvarajkondapalle <66169949+yuvarajkondapalle@users.noreply.github.com>
2021-04-07 14:27:31 +0530

    Create footer.php

---
+++ shopping/footer.php
@@ -0,0 +1,59 @@
+<footer>
+        <div id="foot1">
+            <nav id="foot_nav">
+                <a href="" id="sbc">Shop By Catergory</a>
+                <a href="">Shop By Brand</a>
+                <a href="">Popular Searches</a>
+            </nav>
+            <div id="cate">
+                <table cellspacing="2px" cellpadding="2px">
+                <tr><td>LED Lights</td>
+<td>Paints</td>
+<td>Corporate Gifts</td>
+<td>Geysers</td>
+<td>Welding</td></tr>
+<tr><td>Power tools</td>
+<td>pumps</td>
+<td>Safety</td>
+<td>Bearings</td>
+<td>Cleaning</td></tr>
+<tr><td>Electrical</td>
+<td>Electronics & Robotics</td>
+<td>Fastners</td>
+<td>Furniture, Hospitality and Food</td>
+<td>Service</td></tr>
+<tr><td>Industrial Automation</td>
+<td>Lab Supplies</td>
+<td>Machinery</td>
+<td>Machining Accessories</td>
+<td>Medical Supplies & equipment</td></tr>
+<tr><td>Plumbing</td>
+<td>Pnumeatics</td>
+<td>Security</td></tr>
+                </table>
+            </div>
+            
+        </div>
+        <div id="foot2">
+            <div id="col1">
+                <h3>Company</h3>
+                <a href="">About Us</a>
+                <a href="">Contact Us</a>
+                <a href="">Careers</a>
+                <a href="">Sell On Industry buying</a>
+                <a href="">Articles</a>
+            </div>
+            <div id="col2">
+                <h3>Help</h3>
+                <a href="">FAQs</a>
+                <a href="">Report Infringement</a>
+                <a href="">Ca cellations & Returns</a>
+            </div>
+            <div id="col3">
+                <h2>Subscribe to our Newsletter</h2>
+                <input type="text" placeholder="Enter Email Address" >
+                <button  type="submit" >Sign up</button>
+        
+            </div>
+        </div>
+    </footer>
